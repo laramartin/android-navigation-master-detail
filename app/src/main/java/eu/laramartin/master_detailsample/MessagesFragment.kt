19f7df9038c0a11afc5008fb47bc32822a9105fc
Lara Martin <13904500+laramartin@users.noreply.github.com>
2019-02-24 15:37:14 +0100

    Add Messages and Profile fragments

---
+++ app/src/main/java/eu/laramartin/master_detailsample/MessagesFragment.kt
@@ -0,0 +1,19 @@
+package eu.laramartin.master_detailsample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MessagesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false)
+    }
+}
