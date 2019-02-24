2dcbb277e9d208f41a0db7f346d48a55175172ee
Lara Martin <13904500+laramartin@users.noreply.github.com>
2019-02-24 18:05:47 +0100

    Add account, notifications and settings fragments

---
+++ app/src/main/java/eu/laramartin/master_detailsample/AccountFragment.kt
@@ -0,0 +1,17 @@
+package eu.laramartin.master_detailsample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
