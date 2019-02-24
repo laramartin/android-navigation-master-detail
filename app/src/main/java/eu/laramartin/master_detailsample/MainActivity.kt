8e7c78e368d1254ecd3ac7424b1b6c9612733dee
Lara Martin <13904500+laramartin@users.noreply.github.com>
2019-02-24 15:37:31 +0100

    Navigate to fragments with bottom nav bar

--- app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
+++ app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
@@ -2,11 +2,19 @@ package eu.laramartin.master_detailsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+            .setupWithNavController(navController = navHostFragment.navController)
     }
 }
