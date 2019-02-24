530ead00814779ecffd828da77f0bd5c15e7694c
Lara Martin <13904500+laramartin@users.noreply.github.com>
2019-02-24 18:34:33 +0100

    Add navigation to profile sections fragments

--- app/src/main/java/eu/laramartin/master_detailsample/ProfileFragment.kt
+++ app/src/main/java/eu/laramartin/master_detailsample/ProfileFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 class ProfileFragment : Fragment() {
 
@@ -14,6 +16,18 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.findViewById<TextView>(R.id.account_textview).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.fragment_account)
+        )
+        view.findViewById<TextView>(R.id.notifications_textview).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.fragment_notifications)
+        )
+        view.findViewById<TextView>(R.id.settings_textview).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.fragment_settings)
+        )
+
+        return view
     }
 }
