55cda578c13fd37fa128de9f16d8974a5e953a06
Lara Martin <13904500+laramartin@users.noreply.github.com>
2019-03-09 18:45:44 +0100

    Add up navigation

--- app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
+++ app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
@@ -1,20 +1,35 @@
 package eu.laramartin.master_detailsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
 
         findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
             .setupWithNavController(navController = navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar!!.title = destination.label
+            when (destination.id){
+                R.id.fragment_account,
+                R.id.fragment_settings,
+                R.id.fragment_notifications -> supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+                else -> supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+            }
+        }
     }
 }
